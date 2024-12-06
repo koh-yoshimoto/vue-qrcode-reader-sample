6f7c7ed2f389cc8b69d6c7630ba05a6db0ca3ebc
koh-yoshimoto <yoshimoto.koshiro@gmail.com>
2024-12-06 21:55:06 +0900

    Updated visual

--- src/components/VueQrcodeReader.vue
+++ src/components/VueQrcodeReader.vue
@@ -1,13 +1,11 @@
 <template>
   <div>
     <div>Please scan your QR codes...</div>
-    <QrcodeStream @detect="onDetect"></QrcodeStream>
-		<div class="results">
-			<div class="results-title">Detected QR codes</div>
+    <QrcodeStream @detect="onDetect" :formats="['qr_code']" :track="paintBoundingBox"></QrcodeStream>
+		<div class="result">
+			<div class="result-title">Detected QR codes</div>
 			<ul>
-				<li v-for="(code, index) in codes" :key="index">
-					{{ index + 1 }}: <a :href="code">{{ code }}</a>
-				</li>
+				<li v-for="code,index in result" :key="index">{{index+1}}: {{ code.rawValue }}</li>
 			</ul>
 		</div>
   </div>
@@ -23,24 +21,52 @@ export default defineComponent({
     QrcodeStream,
   },
   setup() {
-    const codes = ref([]);
+    const result = ref([]);
 
     console.log("setup...");
 
-    const onDetect = (result) => {
-      const addedQrCode = result[result.length - 1];
-      console.log("detected...", addedQrCode.rawValue);
-      if (!codes.value.includes(addedQrCode.rawValue)) {
-        codes.value.push(addedQrCode.rawValue);
-      }
-    };
+		const paintOutline = (detectedCodes, ctx) => {
+			for (const detectedCode of detectedCodes) {
+				const [firstPoint, ...otherPoints] = detectedCode.cornerPoints
+
+				ctx.strokeStyle = 'red'
+
+				ctx.beginPath()
+				ctx.moveTo(firstPoint.x, firstPoint.y)
+				for (const { x, y } of otherPoints) {
+					ctx.lineTo(x, y)
+				}
+				ctx.lineTo(firstPoint.x, firstPoint.y)
+				ctx.closePath()
+				ctx.stroke()
+			}
+		}
+
+		const paintBoundingBox = (detectedCodes, ctx) => {
+			for (const detectedCode of detectedCodes) {
+				const {
+					boundingBox: { x, y, width, height }
+				} = detectedCode
+
+				ctx.lineWidth = 2
+				ctx.strokeStyle = '#007bff'
+				ctx.strokeRect(x, y, width, height)
+			}
+		}
+
+    const onDetect = (detectedCodes) => {
+      result.value = detectedCodes
+    }
 
     return {
-      codes,
+      result,
       onDetect,
+      paintOutline,
+			paintBoundingBox
     };
   },
 });
+
 </script>
 
 <style scoped>
@@ -57,13 +83,14 @@ li {
 	text-decoration: underline;
 }
 
-.results-title {
+.result-title {
 	font-size: 1.5em;
 }
 
-.results {
+.result {
 	margin-top: 4px;
 	outline: 2px solid #c2e9a3;
 	border-radius: 2rem;
+	width: 100%;
 }
 </style>
